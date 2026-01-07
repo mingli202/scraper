7cfb9ea4d6ab53914cbdf03cf75228125dc0ced5
mingli202 <vincentmingli@gmail.com>
2026-01-07 13:51:52 -0500

    testing for x_tolerance

--- src/test/new_parser_test.py
+++ src/test/new_parser_test.py
@@ -7,29 +7,82 @@ from models import Word
 from new_parser import NewParser
 
 files = Files()
+width, height = 0, 0
+
 with pdfplumber.open(files.pdf_path) as pdf:
     page = pdf.pages[0]
+    width = page.width
+    height = page.height
     print("width", page.width, "height", page.height)
 
 
+def print_word_position(word: Word) -> str:
+    return f"word: {word.text}, page: {word.doctop / height:.0f}, top: {word.top / height:.2f}, x0: {word.x0 / width:.2f}"
+
+
 @pytest.fixture
-def parser():
+def parser() -> NewParser:
     parser = NewParser(files)
     return parser
 
 
+def test_correct_line_extraction(parser: NewParser) -> None:
+    """
+    Test to make sure the line extraction is correct: whether a word is well separated and the word text is correct
+
+    Parameters:
+        parser (NewParser): The parser object
+
+    Examples cases that it should catch:
+        - 00009 HUMA 345-101-MQ Settler Colonialism and Indigenous Resistance (Blended)MW 1230-1430
+    """
+
+    files = parser.files
+    lines = files.get_sorted_lines_content(False)
+
+    for line_y, line in lines.items():
+        first_word = line[0]
+        line_text = " ".join([word.text for word in line])
+
+        if first_word.text == "SECTION":
+            assert (
+                line_text == "SECTION DISC COURSE NUMBER COURSE TITLE/TEACHER DAY/TIMES"
+            ), "SECTION line should well separated"
+            continue
+
+        if re.match(r"^.*\d{4}-\d{4}$", line_text):
+            assert len(line) >= 2, (line_y, line_text)
+
+            day = line[-2]
+            time = line[-1]
+
+            assert re.match(r"^[MTWRF]+$", day.text) is not None, (
+                "day should only contain letters M, T, W, R, F",
+                line_text,
+                print_word_position(day),
+            )
+            assert re.match(r"^\d{4}-\d{4}$", time.text) is not None, (
+                "time should be in the format of HHMM-HHMM",
+                line_text,
+                print_word_position(time),
+            )
+
+            continue
+
+
 def test_correct_column_x(parser: NewParser):
+    return
     files = parser.files
     lines = files.get_sorted_lines_content()
 
     columns_x: dict[str, set[Word]] = {}
 
-    for line in lines:
+    for line_y, line in lines.items():
         first_word = line[0]
         line_text = " ".join([word.text for word in line])
 
         if first_word.text == "SECTION":
-            assert len(line) == 7, line_text
+            assert len(line) == 7, (line_y, line_text)
 
             columns_x.setdefault("section", set()).add(first_word)
             columns_x.setdefault("disc", set()).add(line[1])
@@ -37,8 +90,8 @@ def test_correct_column_x(parser: NewParser):
             columns_x.setdefault("course_title", set()).add(line[5])
             continue
 
-        if re.search(r"[MTWRF]{1,5}\s+\d{4}-\d{4}", line_text):
-            assert len(line) >= 2, line_text
+        if re.match(r"^.*[MTWRF]{1,5} \d{4}-\d{4}$", line_text):
+            assert len(line) >= 2, (line_y, line_text)
 
             day = line[-2]
             time = line[-1]
@@ -50,7 +103,13 @@ def test_correct_column_x(parser: NewParser):
     assert len({s.x0 for s in columns_x["disc"]}) == 1
     assert len({s.x0 for s in columns_x["course_number"]}) == 1
     assert len({s.x0 for s in columns_x["course_title"]}) == 1
-    assert len({s.x0 for s in columns_x["day"]}) == 1, columns_x["day"]
+
+    for day in columns_x["day"]:
+        if day.x0 == 406:
+            print(day)
+            break
+
+    assert len({s.x0 for s in columns_x["day"]}) == 1
     assert len({s.x0 for s in columns_x["time"]}) == 1
 
     assert columns_x["section"].pop().x0 == parser.columns_x.section
@@ -63,3 +122,29 @@ def test_correct_column_x(parser: NewParser):
 
 def test_basic_parsing(parser: NewParser):
     pass
+
+
+if __name__ == "__main__":
+    files = Files()
+
+    with pdfplumber.open(files.pdf_path) as pdf:
+        page = pdf.pages[93]
+
+        left = 1.5
+        right = 3.0
+        mid = 0
+
+        for i in range(0, 20):
+            mid = (left + right) / 2
+            words = page.extract_words(x_tolerance=1.75)
+            sorted_words = sorted(words, key=lambda w: (w["top"], w["x0"]))
+
+            for word in sorted_words[::-1]:
+                if "(Blended)" in word["text"]:
+                    if "MW" in word["text"]:
+                        right = mid
+                    else:
+                        left = mid
+                    break
+
+            print(mid)
