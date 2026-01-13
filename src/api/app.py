7c81b82625f3af7d5c5565dc2966c2c403513fdc
mingli202 <vincentmingli@gmail.com>
2026-01-12 19:22:27 -0500

    feat: get_sections endpoint

--- src/api/app.py
+++ src/api/app.py
@@ -79,13 +79,13 @@ async def get_sections(
     code: str | None = None,
     title: str | None = None,
     teacher: str | None = None,
-    minRating: int | None = None,
-    maxRating: int | None = None,
-    minScore: int | None = None,
-    maxScore: int | None = None,
-    daysOff: str | None = None,
-    timeStart: str | None = None,
-    timeEnd: str | None = None,
+    min_rating: int | None = None,
+    max_rating: int | None = None,
+    min_score: int | None = None,
+    max_score: int | None = None,
+    days_off: str | None = None,
+    time_start: str | None = None,
+    time_end: str | None = None,
     blended: bool = False,
     honours: bool = False,
 ) -> list[Section]:
@@ -117,9 +117,13 @@ async def get_sections(
     conn = sqlite3.connect(files.all_sections_final_path)
     cursor = conn.cursor()
 
+    rating_conn = sqlite3.connect(files.ratings_db_path)
+    rating_cursor = rating_conn.cursor()
+
     rows = cursor.execute(query, params).fetchall()
 
     sections = [Section.validate_db_response(r) for r in rows]
+    valid_sections: list[Section] = []
 
     for section in sections:
         time_rows = cursor.execute(
@@ -129,11 +133,72 @@ async def get_sections(
             (section.id,),
         ).fetchall()
 
-        section.times = [LecLab.validate_db_response(r) for r in time_rows]
+        times = [LecLab.validate_db_response(r) for r in time_rows]
+
+        valid_time = True
+
+        for time in times:
+            if title is not None and title not in time.title:
+                valid_time = False
+                break
+
+            if teacher is not None and teacher not in time.prof:
+                valid_time = False
+                break
+
+            rating_row = rating_cursor.execute(
+                """
+                SELECT * FROM ratings WHERE prof = ?
+            """,
+                (time.prof,),
+            ).fetchone()
+
+            for d, t in time.time.items():
+                if days_off is not None and d in days_off:
+                    valid_time = False
+                    break
+
+                for t in t:
+                    start_str, end_str = t.split("-")
+
+                    if time_start is not None and int(time_start) < int(start_str):
+                        valid_time = False
+                        break
+                    if time_end is not None and int(time_end) > int(end_str):
+                        valid_time = False
+                        break
+
+            if rating_row is None:
+                valid_time = False
+                break
+
+            try:
+                rating = Rating.validate_db_response(rating_row)
+            except ValidationError:
+                valid_time = False
+                break
+
+            if min_rating is not None and rating.avg < min_rating:
+                valid_time = False
+                break
+            if max_rating is not None and rating.avg > max_rating:
+                valid_time = False
+                break
+
+            if min_score is not None and rating.score < min_score:
+                valid_time = False
+                break
+            if max_score is not None and rating.score > max_score:
+                valid_time = False
+                break
+
+        if valid_time:
+            section.times = times
+            valid_sections.append(section)
 
     conn.close()
 
-    return []
+    return valid_sections
 
 
 @app.get("/ratings/{prof}")
