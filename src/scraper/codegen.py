2a226ee776a69b27db2ae3dba886e5c2e901dd37
mingli202 <vincentmingli@gmail.com>
2026-02-01 15:09:36 -0500

    made some test pass

--- src/scraper/codegen.py
+++ src/scraper/codegen.py
@@ -35,9 +35,9 @@ def main():
                         assert isinstance(node.target, ast.Name)
                         var_name = node.target.id
 
-                        zod_type = handle_annotation(node.annotation)
+                        zod_type = handle_type_annotation(node.annotation)
 
-                        a_zod_type.append(f"{var_name}: {zod_type}")
+                        a_zod_type.append(f"{var_name}: {zod_type},")
 
                 a_zod_type.append("});")
 
@@ -46,31 +46,72 @@ def main():
         print(codegen)
 
 
-def handle_annotation(expr: ast.expr) -> str:
+def handle_type_annotation(expr: ast.expr) -> str:
     match expr:
-        case ast.Name(id):
-            return get_zod_string_from_type(id)
         case ast.BinOp():
-            print(expr)
             return ""
         case ast.Subscript():
-            return ""
+            return handle_subscript(expr)
+        case ast.Name(id):
+            return get_zod_string_from_type(id)
+        case ast.Tuple(elts):
+            return ", ".join(handle_type_annotation(e) for e in elts)
         case _:
             raise Exception(f"Unhandled expr: {expr}")
 
 
+def handle_subscript(subscript: ast.Subscript) -> str:
+    """
+    Returns the zod equivalent of Subscript ast
+
+    Examples:
+    - handle_subscript(parse_ast("dict[int, list[str]]")) -> z.record(z.number(), z.array(z.string()))
+    """
+    assert isinstance(subscript.value, ast.Name)
+
+    name = subscript.value
+    assert isinstance(name, ast.Name)
+
+    args = subscript.slice
+
+    match name.id:
+        case "dict":
+            assert isinstance(args, ast.Tuple)
+            return f"z.record({handle_type_annotation(args.elts[0])}, {handle_type_annotation(args.elts[1])})"
+        case "list":
+            return f"z.array({handle_type_annotation(args)})"
+        case "tuple":
+            return f"z.tuple([{handle_type_annotation(args)}])"
+        case _:
+            raise Exception(f"Unhandled name id {id}")
+
+
 def get_zod_string_from_type(type: str) -> str:
+    """
+    Returns zod equivalent for primitives
+    """
     match type:
         case "float" | "int":
-            return "z.number(),"
+            return "z.number()"
         case "str":
-            return "z.string(),"
+            return "z.string()"
         case _:
-            raise Exception(f"Unhandled type {type}")
+            return type
+
+
+def handle_constant(constant: ast.Constant) -> str:
+    """
+    Returns the zod equivalent for the given python constant
+    """
+    match constant.value:
+        case None:
+            return "z.null()"
+        case _:
+            raise Exception(f"Unhandled constant {constant}")
 
 
 def search_alias_type_definition(type: str) -> str:
-    pass
+    return ""
 
 
 if __name__ == "__main__":
