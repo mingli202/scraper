3148dc76bc6f605c101263ec0e4cf97fc7c5e873
mingli202 <vincentmingli@gmail.com>
2026-02-01 20:03:35 -0500

    split into a separate function

--- src/api/sections/__init__.py
+++ src/api/sections/__init__.py
@@ -1,10 +1,10 @@
 import sqlite3
 from typing import Annotated
 from fastapi import APIRouter, HTTPException, Query
-from pydantic import ValidationError
 
+from api.sections.filter_sections import filter_sections
 from scraper.files import Files
-from scraper.models import LecLab, Rating, Section
+from scraper.models import LecLab, Section
 
 router = APIRouter(prefix="/sections", tags=["Sections"])
 files = Files()
@@ -28,149 +28,23 @@ async def get_sections(
     blended: bool = False,
     honours: bool = False,
 ) -> list[Section]:
-    query = "SELECT * from sections WHERE 1=1"
-    params: list[str] = []
-
-    if q is not None:
-        query += " AND (title LIKE ? OR course LIKE ? OR domain LIKE ? OR code LIKE ?)"
-        params.extend([f"%{q}%", f"{q}%", f"{q}%", f"%{q}%"])
-
-    if title is not None:
-        query += " AND title LIKE ?"
-        params.append(f"%{title}%")
-
-    if course is not None:
-        query += " AND course LIKE ?"
-        params.append(f"{course}%")
-
-    if domain is not None:
-        query += " AND domain LIKE ?"
-        params.append(f"{domain}%")
-
-    if code is not None:
-        query += " AND code LIKE ?"
-        params.append(f"%{code}%")
-
-    if blended:
-        query += " AND more LIKE 'BLENDED%'"
-
-    if honours:
-        query += " AND more LIKE 'For Honours%'"
-
-    conn = sqlite3.connect(files.all_sections_final_path)
-    cursor = conn.cursor()
-
-    rating_conn = sqlite3.connect(files.ratings_db_path)
-    rating_cursor = rating_conn.cursor()
-
-    rows = cursor.execute(query, params).fetchall()
-
-    sections = [Section.validate_db_response(r) for r in rows]
-    valid_sections: list[Section] = []
-
-    for section in sections:
-        time_rows = cursor.execute(
-            """
-            SELECT * FROM times WHERE section_id = ?
-        """,
-            (section.id,),
-        ).fetchall()
-
-        leclabs = [LecLab.validate_db_response(r) for r in time_rows]
-
-        valid_time = True
-
-        if teacher is not None:
-            valid_time = False
-            for leclab in leclabs:
-                if teacher.lower() in leclab.prof.lower():
-                    valid_time = True
-                    break
-
-        if not valid_time:
-            continue
-
-        for leclab in leclabs:
-            for d, t in leclab.time.items():
-                if days_off is not None and any(_d in days_off for _d in d):
-                    valid_time = False
-                    break
-
-                for t in t:
-                    start_str, end_str = t.split("-")
-
-                    if time_start_query is not None and int(start_str) < int(
-                        time_start_query
-                    ):
-                        valid_time = False
-                        break
-                    if time_end_query is not None and int(end_str) > int(
-                        time_end_query
-                    ):
-                        valid_time = False
-                        break
-
-                if not valid_time:
-                    break
-
-            if not valid_time:
-                break
-
-            rating_row = rating_cursor.execute(
-                """
-                SELECT * FROM ratings WHERE prof = ?
-            """,
-                (leclab.prof,),
-            ).fetchone()
-
-            if rating_row is None:
-                valid_time = False
-                break
-
-            try:
-                rating = Rating.validate_db_response(rating_row)
-            except ValidationError:
-                valid_time = False
-                break
-
-            if (
-                min_rating is not None
-                and rating.status == "found"
-                and rating.avg < min_rating
-            ):
-                valid_time = False
-                break
-            if (
-                max_rating is not None
-                and rating.status == "found"
-                and rating.avg > max_rating
-            ):
-                valid_time = False
-                break
-
-            if (
-                min_score is not None
-                and rating.status == "found"
-                and rating.score < min_score
-            ):
-                valid_time = False
-                break
-            if (
-                max_score is not None
-                and rating.status == "found"
-                and rating.score > max_score
-            ):
-                valid_time = False
-                break
-
-        if valid_time:
-            section.times = leclabs
-            valid_sections.append(section)
-
-    conn.close()
-    rating_conn.close()
-
-    return valid_sections
+    return filter_sections(
+        q,
+        course,
+        domain,
+        code,
+        title,
+        teacher,
+        min_rating,
+        max_rating,
+        min_score,
+        max_score,
+        days_off,
+        time_start_query,
+        time_end_query,
+        blended,
+        honours,
+    )
 
 
 @router.get("/{section_id}")
