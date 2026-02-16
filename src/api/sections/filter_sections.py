d989c07aabf22b1f3b8260851259b3f58f8c0a2d
mingli202 <vincentmingli@gmail.com>
2026-02-15 21:47:31 -0500

    using like for everything

--- src/api/sections/filter_sections.py
+++ src/api/sections/filter_sections.py
@@ -1,4 +1,4 @@
-from sqlmodel import select
+from sqlmodel import or_, select
 from scraper.db import SessionDep
 from scraper.models import Section, Status
 
@@ -25,29 +25,31 @@ def filter_sections(
 
     if q is not None:
         statement = statement.where(
-            q in Section.title
-            or q in Section.course
-            or q in Section.domain
-            or q in Section.code
+            or_(
+                Section.title.ilike(f"%{q}%"),
+                Section.course.ilike(f"{q}%"),
+                Section.domain.ilike(f"{q}%"),
+                Section.code.ilike(f"%{q}%"),
+            )
         )
 
     if title is not None:
-        statement = statement.where(title in Section.title)
+        statement = statement.where(Section.title.ilike(f"%{title}%"))
 
     if course is not None:
-        statement = statement.where(course in Section.course)
+        statement = statement.where(Section.course.ilike(f"{course}%"))
 
     if domain is not None:
-        statement = statement.where(domain in Section.domain)
+        statement = statement.where(Section.domain.ilike(f"{domain}%"))
 
     if code is not None:
-        statement = statement.where(code in Section.code)
+        statement = statement.where(Section.code.ilike(f"%{code}%"))
 
     if blended:
-        statement = statement.where(Section.more.startswith("BLENDED"))
+        statement = statement.where(Section.more.ilike("BLENDED%"))
 
     if honours:
-        statement = statement.where(Section.more.startswith("For Honours"))
+        statement = statement.where(Section.more.ilike("For Honours%"))
 
     sections = session.exec(statement)
     valid_sections: list[Section] = []
