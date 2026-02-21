c2b437c6a8b7e509116a0d56eda17d0b796ce967
mingli202 <vincentmingli@gmail.com>
2026-02-21 12:07:22 -0500

    reference

--- src/api/playground.py
+++ src/api/playground.py
@@ -1,14 +1,37 @@
 from sqlmodel import Session, select
 from scraper.db import engine
-from scraper.models import LecLab, Section
+from scraper.models import DayTime, LecLab, LecLabType, Section
 
 
 def main():
-    with Session(engine) as session:
-        section = session.get(Section, 0)
+    section = Section.default(
+        section="00001",
+        code="609-DAA-03",
+        title="German I",
+        leclabs=[
+            LecLab.default(
+                title="German I",
+                prof="Siderova, Spaska",
+                day_times=[
+                    DayTime(
+                        day="TR",
+                        start_time_hhmm="1300",
+                        end_time_hhmm="1430",
+                    )
+                ],
+                type=LecLabType.LECTURE,
+            )
+        ],
+    )
 
-        if section is None:
-            return
+    print(section.leclabs)
+
+
+# with Session(engine) as session:
+#     section = session.get(Section, 0)
+#
+#     if section is None:
+#         return
 
 
 if __name__ == "__main__":
