28bc553ea445662f8aea0f00213abcbcbc682dd7
mingli202 <vincentmingli@gmail.com>
2026-02-21 22:19:12 -0500

    hmm ok

--- src/api/playground.py
+++ src/api/playground.py
@@ -1,6 +1,6 @@
 from sqlmodel import Session, select
 from scraper.db import engine
-from scraper.models import DayTime, LecLab, LecLabType, Section
+from scraper.models import DayTime, LecLab, LecLabType, Rating, Section
 
 
 def main():
@@ -10,10 +10,12 @@ def main():
         if section is None:
             return
 
-        print(section)
-        print(section.leclabs)
+        # print(section)
+        # print(section.leclabs)
+        #
+        # print([leclab.day_times for leclab in section.leclabs])
 
-        print([leclab.day_times for leclab in section.leclabs])
+        print(session.get(Rating, "Dupont, Sarah"))
 
 
 if __name__ == "__main__":
