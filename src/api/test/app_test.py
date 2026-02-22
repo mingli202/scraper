d5894727cfe1c3a16d840e5af2ccaa9f1393e951
mingli202 <vincentmingli@gmail.com>
2026-02-21 22:19:16 -0500

    test perchance

--- src/api/test/app_test.py
+++ src/api/test/app_test.py
@@ -3,7 +3,15 @@ import pytest
 from api.app import app
 from fastapi.testclient import TestClient
 
-from scraper.models import DayTime, LecLab, LecLabType, Rating, Section, Status
+from scraper.models import (
+    DayTime,
+    LecLab,
+    LecLabType,
+    Rating,
+    Section,
+    SectionResponse,
+    Status,
+)
 
 client = TestClient(app)
 
@@ -18,73 +26,40 @@ def test_get_all_sections():
     res = client.get("/sections/")
     assert res.status_code == 200
     assert len(res.json()) > 900
-    first_section = Section.model_validate(res.json()[0])
-    assert first_section == Section(
-        id=0,
-        course="Science Courses",
-        section="00001",
-        domain="BIOLOGY",
-        code="101-SN1-RE",
-        title="Cellular Biology",
-        leclabs=[
-            LecLab(
-                section_id=0,
-                title="Cellular Biology",
-                type=LecLabType.LECTURE,
-                prof="Dupont, Sarah",
-                day_times=[
-                    DayTime(day="R", start_time_hhmm="0930", end_time_hhmm="1130")
-                ],
-            ),
-            LecLab(
-                section_id=0,
-                title="Cellular Biology",
-                type=LecLabType.LAB,
-                prof="Hughes, Cameron",
-                day_times=[
-                    DayTime(day="T", start_time_hhmm="1230", end_time_hhmm="1430")
-                ],
-            ),
-        ],
-        more="",
-        view_data=[{"4": [4, 8]}, {"2": [10, 14]}],
-    )
 
 
 def test_get_section():
     res = client.get("/sections/1")
     assert res.status_code == 200
-    section = Section.model_validate(res.json())
-    assert section == Section(
-        id=1,
-        course="Science Courses",
-        section="00002",
-        domain="BIOLOGY",
-        code="101-SN1-RE",
-        title="Cellular Biology",
-        leclabs=[
-            LecLab(
-                section_id=1,
-                title="Cellular Biology",
-                type=LecLabType.LECTURE,
-                prof="Rioux, Marie-Claire",
-                day_times=[
-                    DayTime(day="T", start_time_hhmm="1430", end_time_hhmm="1630")
-                ],
-            ),
-            LecLab(
-                section_id=1,
-                title="Cellular Biology",
-                type=LecLabType.LAB,
-                prof="Dupont, Sarah",
-                day_times=[
-                    DayTime(day="R", start_time_hhmm="0930", end_time_hhmm="1130")
-                ],
-            ),
-        ],
-        more="",
-        view_data=[{"4": [4, 8]}, {"2": [14, 18]}],
-    )
+    section = SectionResponse.model_validate(res.json())
+
+    assert section.course == "Science Courses"
+    assert section.section == "00002"
+    assert section.domain == "BIOLOGY"
+    assert section.code == "101-SN1-RE"
+    assert section.title == "Cellular Biology"
+    assert section.more == ""
+    assert section.view_data == [{"4": [4, 8]}, {"2": [14, 18]}]
+
+    assert len(section.leclabs) == 2
+
+    l1, l2 = section.leclabs
+    assert l1.section_id == l2.section_id == 1
+    assert l1.title == l2.title == "Cellular Biology"
+
+    assert l1.type == LecLabType.LECTURE
+    assert l1.prof == "Dupont, Sarah"
+    assert len(l1.day_times) == 1
+    assert l1.day_times[0].day == "R"
+    assert l1.day_times[0].start_time_hhmm == "0930"
+    assert l1.day_times[0].end_time_hhmm == "1130"
+
+    assert l2.type == LecLabType.LAB
+    assert l2.prof == "Rioux, Marie-Claire"
+    assert len(l2.day_times) == 1
+    assert l2.day_times[0].day == "T"
+    assert l2.day_times[0].start_time_hhmm == "1430"
+    assert l2.day_times[0].end_time_hhmm == "1630"
 
 
 @pytest.mark.parametrize("id", [-1, 10000, "nan", None])
