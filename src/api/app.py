3ec70a01cccdd32724a1310cc7ab32ffc6ee5fe6
mingli202 <vincentmingli@gmail.com>
2026-07-27 18:02:25 -0400

    wtf

--- src/api/app.py
+++ src/api/app.py
@@ -58,22 +58,6 @@ async def get_global_all_sections() -> GlobalAllSections:
     files = Files()
     globalAllSections = files.get_global_all_sections_content()
 
-    sections = globalAllSections.sections_by_id
-    removedSection = sections["101-A1S-AB-00001"]
-    del sections["101-A1S-AB-00001"]
-
-    originalSection = sections["345-2F4-AB-00001"]
-    changedSection = originalSection.model_copy(deep=True)
-
-    originalSection.leclabs[0].prof = "new prof"
-    originalSection.title = "new title"
-    originalSection.leclabs.append(changedSection.leclabs[0])
-
-    sections_diff = globalAllSections.sections_diff
-    if sections_diff is not None:
-        sections_diff.sections_removed.append(removedSection)
-        sections_diff.previous_sections_changed.append(changedSection)
-
     return globalAllSections
 
 
